a883f06ac1d7771a5d25ccd60b0cc1c493af622c
Akiva Leffert <aleffert@gmail.com>
2019-01-03 14:13:08 -0500

    Add load balancer frontend

--- infrastructure/terraform/bucket.tf
+++ infrastructure/terraform/bucket.tf
@@ -1,5 +1,9 @@
+locals {
+    component_name = "${var.app_name}-frontend"
+}
+
 resource "google_storage_bucket" "static_site" {
-  name     = "${var.app_name}-frontend"
+  name     = "${local.component_name}"
 
   website {
     main_page_suffix = "index.html"
@@ -8,13 +12,24 @@ resource "google_storage_bucket" "static_site" {
 }
 
 resource "google_compute_backend_bucket" "static_backend" {
-  name        = "${var.app_name}-backend-bucket"
+  name        = "${local.component_name}-backend-bucket"
   description = "Backend bucket for frontend of ${var.app_name}"
   bucket_name = "${google_storage_bucket.static_site.name}"
   enable_cdn  = true
 }
 
 resource "google_compute_url_map" "static_map" {
-  name            = "${var.app_name}-lb"
+  name            = "${local.component_name}-lb"
   default_service = "${google_compute_backend_bucket.static_backend.self_link}"
+}
+
+resource "google_compute_global_forwarding_rule" "ipv4-https" {
+  name       = "${local.component_name}-https"
+  target     = "${google_compute_backend_bucket.static_backend.self_link}"
+  ip_address = "${google_compute_global_address.ipv4.address}"
+  port_range = "443"
+  depends_on = ["google_compute_global_address.default"]
+}
+resource "google_compute_global_address" "ipv4" {
+  name       = "${var.app_name}-fe-address"
 }
