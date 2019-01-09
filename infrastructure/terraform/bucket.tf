e63bd2af4e441a08436a2584587cea7f3257eacd
Akiva Leffert <aleffert@gmail.com>
2019-01-08 19:57:01 -0500

    Trying to share lb with front and backend was a bad idea

--- infrastructure/terraform/bucket.tf
+++ infrastructure/terraform/bucket.tf
@@ -23,3 +23,25 @@ resource "google_compute_backend_bucket" "static_backend" {
   bucket_name = "${google_storage_bucket.static_site.name}"
   enable_cdn  = true
 }
+
+resource "google_compute_url_map" "static_map" {
+  name            = "${local.component_name}-lb"
+  default_service = "${google_compute_backend_bucket.static_backend.self_link}"
+}
+
+resource "google_compute_target_https_proxy" "static_ipv4_https_proxy" {
+    name = "${local.component_name}-proxy"
+    url_map = "${google_compute_url_map.static_map.self_link}"
+    ssl_certificates = ["${var.app_name}-root"]
+}
+
+resource "google_compute_global_forwarding_rule" "ipv4-https" {
+  name       = "${local.component_name}-https"
+  target     = "${google_compute_target_https_proxy.static_ipv4_https_proxy.self_link}"
+  ip_address = "${google_compute_global_address.ipv4.address}"
+  port_range = "443"
+}
+
+resource "google_compute_global_address" "ipv4" {
+  name       = "${var.app_name}-fe-address"
+}
