ee49dc71541085d28c48ff5da097d6e030c4b7a1
Akiva Leffert <aleffert@gmail.com>
2019-01-13 20:13:00 -0500

    Add redux dev tools integration

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import './App.css';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from 'redux-saga';
 
 import { LanguageContext } from './utils';
 import { Root } from './Root';
 import { reducers, saga } from './store';
 
+
+
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(saga);
 
